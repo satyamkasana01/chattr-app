d251438691ab507f5f0e1e5b89dd1f099e533843
satyamkasana01 <satyamkasana31@gmail.com>
2026-06-15 02:17:04 +0530

    add RabbitMQ message publishing functionality

--- backend/user/src/confing/rabbitmq.ts
+++ backend/user/src/confing/rabbitmq.ts
@@ -1,4 +1,5 @@
 import amqplib from 'amqplib'
+import { json } from 'node:stream/consumers';
 
 let channel: amqplib.Channel;
 
@@ -18,4 +19,17 @@ export const connectRabbitMq = async() => {
     } catch (error) {
         console.log("Failed to connect to rabbitmq", error)
     }
+}
+
+export const publishToQueue = async (queueName: string, message: any) => {
+    if (!channel) {
+        console.log("Rabbit channel is not intialized");
+        return;
+    }
+
+    await channel.assertQueue(queueName, { durable: true });
+
+    channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)), {
+        persistent: true,
+    });
 }
