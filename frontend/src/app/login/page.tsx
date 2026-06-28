b6212fa5090d4c653594a6e21f55b6d5c7a32449
satyamkasana01 <satyamkasana31@gmail.com>
2026-06-29 01:16:42 +0530

    feat: add login page with email input and verification functionality

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,66 @@
+"use client"
+import axios from 'axios'
+import { ArrowRight, Mail } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
+
+const LoginPage = () => {
+    const [email, setEmail] = useState<string>('')
+    const [loading, setLoading] = useState<boolean>(false)
+    const router = useRouter()
+
+    const handleSubmit = async(e: React.FormEvent<HTMLFormElement>): Promise<void> => {
+        e.preventDefault()
+        setLoading(true)
+        try {
+            const {data} = await axios.post(`http://localhost:5000/api/v1/login`, {email})
+            alert(data.message)
+            router.push(`/verify?email=${email}`) // Redirect to the verification page with the email as a query parameter
+        } catch (error: any) {
+            alert(error.response.data.error)
+        } finally {
+            setLoading(false)
+        }
+    }
+  return (
+    <div className='min-h-screen bg-gray-900 flex items-center justify-center p-4'>
+      <div className='max-w-md w-full'>
+        <div className='bg-gray-800 border border-gray-700 rounded-lg p-8'>
+            <div className='text-center mb-8'>
+              <div className='mx-auto w-20 h-20 bg-blue-600 rounded-lg flex items-center justify-center mb-6'>
+                <Mail size={40} className='text-white' />
+              </div>
+              <h1 className='text-4xl font-bold text-white mb-3'>Welcome to ChatApp</h1>
+              <p className='text-gray-300 text-lg'>Enter your email to continue your journey with us.</p>
+            </div>
+            <form onSubmit={handleSubmit} className='space-y-6'>
+                <div>
+                    <label htmlFor="email" className='block text-sm font-medium text-gray-300 mb-2'>
+                        Email Adress
+                    </label>
+                    <input 
+                    type="email" 
+                    id='email' 
+                    className='w-full px-4 py-4 bg-gray-700 border border-gray-600 rounded-lg text-white placeholder-gray-400' 
+                    placeholder='Enter Your email address'
+                    value={email} // Bind the input value to the email state
+                    onChange={(e) => setEmail(e.target.value)} // Update the email state on input change
+                    required 
+                    />
+                </div>
+                <button
+                type='submit'
+                className='w-full bg-blue-600 text-white py-4 px-6 rounded-lg font-semibold hover:bg-blue-700 disable:opacity-50 disabled:cursor-not-allowed ' >
+                <div className='flex items-center justify-center gap-2'>
+                    <span>Send Verification Code</span>
+                    <ArrowRight className='w-5 h-5' />
+                </div>
+                </button>
+            </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage
