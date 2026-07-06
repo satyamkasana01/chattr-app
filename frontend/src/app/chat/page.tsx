f01399719cddf12cd9d13c86a7709915609c8144
satyamkasana01 <satyamkasana31@gmail.com>
2026-07-06 21:36:14 +0530

    feat: implement ChatSidebar component; enhance chat user selection and sidebar functionality

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -1,24 +1,64 @@
 "use client"
 
+import ChatSidebar from '@/src/components/ChatSidebar'
 import Loading from '@/src/components/Loading'
-import { useAppData } from '@/src/context/AppContext'
+import { useAppData, User } from '@/src/context/AppContext'
 import { useRouter } from 'next/navigation'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+
+export interface Message {
+  id: string;
+  chatId: string;
+  sender: string;
+  text?: string;
+  image?:{
+    url: string;
+    publicId: string;
+  };
+  messageType: "text" | "image";
+  seen: boolean;
+  seenAt?: string;
+  createdAt: string;
+}
 
 const page = () => {
-  const {loading, isAuth}= useAppData()
-  const router = useRouter()
+  const {loading, isAuth, logoutUser, chats, user:loggedInUser, users, fetchChats, setChats}= useAppData()   // loggedInUser = me / current logged-in account
 
+  const[selectedUser, setSelectedUser] = useState<string | null>(null)
+  const [message, setMessage] = useState("") // for the message input field
+  const [sidebarOpen, setSidebarOpen] = useState<boolean>(false)
+  const [messages, setMessages] = useState<Message[] | null>(null) // for the messages of the selected chat
+  const [user, setUser] = useState<User | null>(null) // selected person I am chatting with
+  const [showAllUser, setShowAllUser] = useState<boolean>(false)
+  const [isTyping, setIsTyping] = useState<boolean>(false)
+  const [typingTimeout, setTypingTimeout] = useState<NodeJS.Timeout | null>(null)
+
+
+  const router = useRouter()
+ 
   useEffect(()=> {
     if(!isAuth && !loading){
       router.push("/login")
     }
   },[isAuth, router, loading])
 
+  const handleLogout = () =>  logoutUser()
+
   if(loading) return <Loading />;
   return (
-    <div>
-      ChatApp
+    <div className='min-h-screen flex bg-gray-900 text-white relative overflow-hidden'>
+      <ChatSidebar
+        sidebarOpen={sidebarOpen}
+        setSidebarOpen={setSidebarOpen}
+        showAllUser={showAllUser}
+        setShowAllUser={setShowAllUser}
+        users={users}
+        loggedInUser={loggedInUser}
+        chats={chats}
+        selectedUser={selectedUser}
+        setSelectedUser={setSelectedUser}
+        handleLogout={handleLogout}
+      />
     </div>
   )
 }
---
+++ frontend/src/components/ChatSidebar.tsx
@@ -0,0 +1,132 @@
+import React, { useState } from 'react'
+import { User } from '../context/AppContext';
+import { MessageCircle, Plus, Search, UserCircle, X } from 'lucide-react';
+
+interface ChatSidebarProps {
+    sidebarOpen: boolean;
+    setSidebarOpen: (open: boolean) => void;
+    showAllUser: boolean;
+    setShowAllUser: (show: boolean | ((prev: boolean) => boolean)) => void;
+    users: User[] | null;
+    loggedInUser: User | null;
+    chats: any[] | null;
+    selectedUser: string | null;
+    setSelectedUser: (userId: string | null) => void;
+    handleLogout: () => void;
+} 
+
+const ChatSidebar = ({sidebarOpen, setSidebarOpen, showAllUser, setShowAllUser, users, loggedInUser, chats, selectedUser, setSelectedUser, handleLogout}: ChatSidebarProps)=> {
+   const [searchQuery, setSearchQuery] = useState("")
+  return (
+    <aside className={`fixed z-20 sm:static top-0 left-0 h-screen w-80 bg-gray-900 border-r border-gray-700 transform ${sidebarOpen?"translate-x-0":"-translate-x-full"} sm:translate-x-0 transition-transform duration-300 flex flex-col`}>
+      {/*header*/}
+      <div className='p-6 border-b border-gray-700'>
+        <div className='sm:hidden flex justify-end mb-0'>
+          <button onClick={() => setSidebarOpen(false)} className='p-2 hover:bg-gray-700 rounded-lg transition-colors'>
+            <X className='w-5 h-5 text-gray-300'/>
+          </button>
+        </div>
+        <div className='flex items-center justify-between'>
+          <div className='flex items-center gap-3'>
+            <div className='p-2 bg-blue-600 justify-between'>
+              <MessageCircle className='w-5 h-5 text-white' />
+            </div>
+            <h2 className='text-xl font-bold text-white'>
+              {showAllUser? "New Chat":"Messages"}
+            </h2>
+          </div>
+          <button className={`p-2.5 rounded-lg transition-colors ${showAllUser? "bg-red-600 hover:bg-red-700 text-white":"bg-green-600 hover:bg-green-700 text-white"}`}
+          onClick={()=> setShowAllUser((prev)=> !prev)}
+          >
+            {
+              showAllUser? <X className='w-4 h-4'/> : <Plus className='w-4 h-4' />
+            } 
+          </button>
+        </div>
+      </div>
+      {/*content*/}
+      <div className='flex-1 overflow-hidden px-4 py-2'>
+        {
+          showAllUser? <div className='space-y-4 h-full'>
+            <div className='relative'>
+              <Search className='absolute left-3 top-1/2 transform -translate-y-1/2 w-e4 h-4 text-gray-400'/> 
+              <input 
+                type="text" 
+                placeholder="Search users..." 
+                className="w-full pl-10 pr-4 py-3 bg-gray-800 border border-gray-700 text-white placeholder-gray-400 "
+                value={searchQuery} 
+                onChange={(e) => setSearchQuery(e.target.value)}
+              />
+            </div>
+            <div className='space-y-2 overflow-y-auto w-full h-full pb-4'>
+              {
+                users?.filter((u) => u._id !== loggedInUser?._id && u.name.toLowerCase().includes(searchQuery.toLocaleLowerCase())).map((u) => (
+                  <button key={u._id} className='w-full text-left p-4 rounded-lg border border-gray-700 hover:border-gray-600 hover:bg-gray-800 transition-colors'>
+                    <div className='flex items-center gap-3'>
+                      <div className='relative'>
+                        <UserCircle className='w-6 h-6 text-gray-300' />
+                      </div>
+                      {/*online symbol dikhna*/}
+                       <div className='flex-1 min-w-0'>
+                      <span className='font-medium  text-white'>{u.name}</span>
+                      <div className='text-xs text-gray-400 mt-0.5'>
+                        {/* to show online offline text */}
+                      </div>
+                    </div>
+                    </div>
+                  </button>
+                ))
+              }
+ 
+            </div>
+              
+          </div>: chats && chats.length > 0? <div className='space-y-2 overflow-y-auto h-full pb-4'>
+            {
+              chats.map((chat)=>{
+                const latestMessage = chat.chat.latestMessage;
+                const isSelected = selectedUser === chat.chat._id;
+                const isSentByme = latestMessage?.sender == loggedInUser?._id;
+                const unseentCount = chat.chat.unseenCount|| 0;
+
+                return <button key={chat.chat_id} 
+                onClick={()=>{
+                  setSelectedUser(chat.chat._id)
+                  setSidebarOpen(false)
+                }}
+                className={`w-full text-left p-4 rounded-lg transition-colors ${isSelected? "bg-blue-600 border-blue-500":"border-gray-700 hover:bg-gray-600"}`}
+                >
+                  <div className='flex items-center gap-3'>
+                    <div className="relative">
+                      <div className='w-12 h-12 rounded-full bg-gray-700 flex items-center justify-center'>
+                        <UserCircle className='w-7 h-7 text-gray-300' />
+                        {/* online user work */}
+                      </div>
+                      <div className='flex-1 min-w-0'>
+                        <div className='flex iteam-center justify-between mb-1'>
+                          <span className={`font-semibold truncate ${
+                            isSelected? "text-white":"text-gray-200"
+                          }`}>
+                            {chat.user.name}
+                          </span>
+                          {
+                            unseentCount > 0 && <div className='bg-red-600 text-white text-xs font-bold rounded-full min-w-5.5 h-5.5 flex items-center justify-center px-2'>
+                              {
+                                unseentCount > 99 ? "99+": unseentCount
+                              }
+                            </div>
+                          }
+                        </div>
+                      </div>
+                    </div>
+                  </div>  
+                </button>
+              })
+            }
+          </div> : <div></div>
+        }
+      </div>
+    </aside>
+  )
+}
+
+export default ChatSidebar 
